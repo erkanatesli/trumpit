53849a8f2ce5dca48ec771229d2f6cd36deb514d
Nicolas Krul <nicolaskrul@googlemail.com>
2017-02-10 10:42:32 +0100

    dark theme

--- trumpit/src/pages/unlock/unlock.ts
+++ trumpit/src/pages/unlock/unlock.ts
@@ -15,23 +15,13 @@ export class Unlock {
 
   contacts: Array<string>;
   numberOfContacts: number;
+  gpsIcon: string = "locate";
+  fpIcon: string = "finger-print";
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private uploadProvider: UploadProvider) {
     // If we navigated to this page, we will have an item available as a nav param
     // this.selectedItem = navParams.get('item');
-    //
-    // // Let's populate this page with some filler content for funzies
-    // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    // 'american-football', 'boat', 'bluetooth', 'build'];
-    //
-    // this.items = [];
-    // for (let i = 1; i < 11; i++) {
-    //   this.items.push({
-    //     title: 'Item ' + i,
-    //     note: 'This is item #' + i,
-    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    //   });
-    // }
+
 
     this.contacts = this.uploadProvider.getContacts();
     this.numberOfContacts = this.contacts.length;
